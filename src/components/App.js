56b8dc33c72d109a9b1428421fea2b592234871a
ktkim <ktkim0111@gmail.com>
2021-08-07 17:02:27 +0900

    env ignore에서 제거 email auth 작업중

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,8 @@
-import React, { useState } from "react";
-import AppRouter from "components/Router";
-import { authService } from "fBase";
+import React, { useState } from 'react';
+import AppRouter from 'components/Router';
+import { authService } from 'fBase';
 
 function App() {
-  console.log(authService.currentUser);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   return <AppRouter isLoggedIn={isLoggedIn} />;
 }
--- src/components/Router.js
+++ src/components/Router.js
@@ -1,7 +1,6 @@
-import React, { useState } from "react";
-import { HashRouter as Router, Route, Switch } from "react-router-dom";
-import Auth from "../routes/Auth";
-import Home from "../routes/Home";
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+import Auth from '../routes/Auth';
+import Home from '../routes/Home';
 
 const AppRouter = ({ isLoggedIn }) => {
   return (
